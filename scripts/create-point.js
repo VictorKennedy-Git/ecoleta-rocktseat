8e90d749ae666db7557c3b3989d9d037aef65af6
kennedy <victorsilvasm92@gmail.com>
2023-02-17 13:36:02 -0300

    Formulário Cidade JS, verificar Bug do selec Cidades

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,12 +1,37 @@
-function populateUFs(){
-    let stateselect = document.querySelector('select[name="uf"]')
+let stateselect = document.querySelector('select[name="uf"]')
+
+function populateUFs() {
     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-    .then(res => res.json()).then(states => {
-        for(state of states){
-            stateselect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
-        }
-    })
+        .then(res => res.json()).then(states => {
+            for (state of states) {
+                stateselect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+            }
+        })
+
 }
 
+function cityEnable(change) {
+    let selectCity = document.querySelector('select[name="city"]')
+
+        selectCity.disabled = false
+
+        let disabledoption = document.querySelector('option[name="selecionar"]')
+        disabledoption.setAttribute('disabled', true)
+    
+     let idcity = change.target.value
+        localStorage.setItem('id', idcity)
+        let url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${localStorage.getItem('id')}/municipios`
+
+        fetch(url).then((res) => res.json()).then(cities => {
+            for (city of cities) {
+                selectCity.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            }
+    })
+    
+}
 
 populateUFs()
+
+var change = stateselect
+change.addEventListener('change', cityEnable)
+
