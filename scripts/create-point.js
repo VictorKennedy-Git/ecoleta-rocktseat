b7715a17fdeda3c00f94ba09cb18c5855419a5b4
kennedy <victorsilvasm92@gmail.com>
2023-03-01 10:38:43 -0300

    Retornando

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -10,31 +10,31 @@ function populateUFs() {
 
 }
 
-function cityEnable(change, event) {
-    let selectCity = document.querySelector('select[name="city"]')
-   
+let selectCity = document.querySelector('select[name="city"]')
 
-        selectCity.disabled = false
+function cityEnable(change) {
+    selectCity.disabled = false //habilita select de cidade.
 
-        let disabledoption = document.querySelector('option[name="selecionar"]')
-        disabledoption.setAttribute('disabled', true)
-    
+    //SELECIONA E DESABITITA SELECT DE VALUE 1 NO CAMPO DE ESTADO
+    let disabledoption = document.querySelector('option[name="selecionar"]')
+    disabledoption.setAttribute('disabled', true)
     const stateInput = document.querySelector('input[name=state]')
     const indexOfSelectedState = change.target.selectedIndex
-    
+
     stateInput.value = change.target.options[indexOfSelectedState].text
 
     let idcity = change.target.value
 
-        localStorage.setItem('id', idcity)
-        let url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${localStorage.getItem('id')}/municipios`
+    localStorage.setItem('id', idcity)
+    let url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${localStorage.getItem('id')}/municipios`
 
-        fetch(url).then((res) => res.json()).then(cities => {
-            selectCity.innerHTML = "<option>Selecionar Cidade</option>"
-            for (city of cities) {
-                selectCity.innerHTML += `<option value="${city.id}">${city.nome}</option>`
-            }
+    fetch(url).then((res) => res.json()).then(cities => {
+        selectCity.innerHTML = "<option value='0'>Selecionar Cidade</option>"
+        for (city of cities) {
+            selectCity.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
+        }
     })
+
 }
 
 populateUFs()
@@ -42,3 +42,34 @@ populateUFs()
 var change = stateselect
 change.addEventListener('change', cityEnable)
 
+//ITENS DE COLETA
+const itenToCollect = document.querySelectorAll(".itens-grid li")
+
+for (const item of itenToCollect) {
+    item.addEventListener('click', RandomSelectdItem)
+}
+
+let selectedItems = []
+
+function RandomSelectdItem(event) {
+    const itemLi = event.target
+
+    const itemId = itemLi.dataset.id
+
+    itemLi.classList.toggle('selected') //ADICIONAR OU REMOVER
+
+
+    //VERIFICAR SE TEM ITENS SELECIONADOS, 
+    const alreadySelected = selectedItems.findIndex(function(item){
+        const itemFound = item == itemId
+        return itemFound
+    })
+    //SE SIM PEGAR OS ITENS SELECIONASDO
+
+
+    //SE  JA TIVER SELECIONADO, TIRAR DA XELECAO
+
+    //SE NÃO TIVER SELECIONADO ADICIOAR A SELEÇAO
+
+    //ARUALIZAR O CAMPO EXONDIDO COM OS ITENS SELECIONASOS
+}
